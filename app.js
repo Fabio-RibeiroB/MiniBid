0dbd3810e9c9e5f5f00c78c4d11787afd0a7dfc0
Fabio-RibeiroB <fabio.brady@gmail.com>
2022-02-10 11:33:21 +0000

    Server is running with Homepage working

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+// const mongoose = require('mongoose')
+//const bodyParser = require('body-parser')
+// require('dotenv/config')
+
+// app.use(bodyParser.json())
+
+// Middleware
+// const authRoute = require('./routes/auth') // user authenticatoin
+// const auctionRoute = require('./routes/auth) // auctions
+// const itemsRoute = require('./routes/items) // items
+
+
+// Routes
+// app.use('/api/user', authRoute)
+// app.use('/api/auctions', auctionRoute)
+// app.use('/api/items', itemsRoute)
+app.get('/', (req,res) =>{
+    res.send('Homepage!')
+})
+
+
+// Connect DB
+// mongoose.connect(process.env.DB_CONNECTOR, ()=>{
+//     console.log('DB is connected')
+// })
+
+// Start the server
+app.listen(3000, ()=>{
+    console.log('Server is running...')
+})
